6fbf16363c4bdc12ad5eefe4094ba3ab8c0115dc
veronicasouza <veronicasp54@gmail.com>
2026-01-12 16:14:53 -0300

    refactor: otimizando addArguments

--- src/test/java/utils/DriverManager.java
+++ src/test/java/utils/DriverManager.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
 import java.util.Collections;
 
 public class DriverManager {
@@ -19,20 +20,12 @@ public class DriverManager {
             // Remove a flag "controlled by automated software"
             options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
             options.setExperimentalOption("useAutomationExtension", false);
-            options.addArguments("--disable-dev-shm-usage"); // Melhora estabilidade em Docker/Linux
-            options.addArguments("--disable-browser-side-navigation");
-
-            // Adiciona argumentos para parecer navegador comum
-            options.addArguments("--disable-blink-features=AutomationControlled");
-            options.addArguments("--start-maximized");
-            options.addArguments("--incognito"); // Ajuda a evitar rastreios de sessões anteriores
-
-            // Define um User-Agent real
-            options.addArguments("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36");
-
-            // Substitua o caminho do computador
-            options.addArguments("--user-data-dir=/Users/veronicasouza/Library/Application Support/Google/Chrome/Profile 1");
-            options.addArguments("--profile-directory=Default");
+            options.addArguments("--disable-dev-shm-usage",
+                    "--disable-browser-side-navigation",
+                    "--disable-blink-features=AutomationControlled",
+                    "--start-maximized", "--incognito", "user-agent= Chrome/143.0.7499.193",
+                    "--user-data-dir=/Users/veronicasouza/Library/Application Support/Google/Chrome/Profile 1",
+                    "--profile-directory=Profile1");
 
             driver.set(new ChromeDriver(options));
         }
