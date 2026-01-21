7d9bbd58cdf296f9f70ad6e279e573369fcad7a1
veronicasouza <veronicasp54@gmail.com>
2026-01-21 11:30:04 -0300

    refactor: retirar thread spleeps

--- src/test/java/utils/Hooks.java
+++ src/test/java/utils/Hooks.java
@@ -2,8 +2,11 @@ package utils;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.HomePage;
+
+import java.time.Duration;
 
 public class Hooks {
 
@@ -13,17 +16,14 @@ public class Hooks {
         homePage.navigateTo();
     }
 
-    // este roda após cada cenário
     @After(order = 1)
     public void afterScenario(io.cucumber.java.Scenario scenario) {
-        try {
-            Thread.sleep(2000);
-            // espera 2 segundos
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        new WebDriverWait(DriverManager.getDriver(), Duration.ofSeconds(10))
+                .until(d -> d instanceof TakesScreenshot);
+
         Screenshot.saveScreenshot(DriverManager.getDriver(), scenario.getName() + ".png");
     }
+
 
     // este roda depois, para encerrar o driver
     @After(order = 0)
